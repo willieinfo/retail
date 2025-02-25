459fcf82330d312058107b9f0151c2297beed5d9
willie <willie.estrada@regent-trg.com>
2025-02-25 09:22:41 +0800

    Updated files and fixed .gitignore

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -12,6 +12,45 @@ img.style.objectFit = "cover";
 img.style.zIndex = "-1";  
 
 // MainMenu
+const menuItems= `
+<li class="sales"><i class="fa fa-money"></i> Sales
+    <ul class="dropdown submenu">
+        <li class="salesRankingByLocation">Sales Ranking by Location</li>
+        <li class="salesRankingByBrand">Sales Ranking by Brand</li>
+        <li>Daily Sales Summary</li>
+    </ul>
+</li>
+<li class="purchases">Purchases
+    <ul class="dropdown submenu">
+        <li>By SKU</li>
+        <li>By Category</li>
+        <li>By Supplier</li>
+    </ul>
+</li>
+<li class="transfers">Transfers
+    <ul class="dropdown submenu">
+        <li>Stock Transfer by SKU</li>
+        <li>Stock Transfer by Category</li>
+    </ul>
+</li>
+<li class="inventory">Inventory
+    <ul class="dropdown submenu">
+        <li>Stock Ending</li>
+        <li>Stock Position</li>
+    </ul>
+</li>
+<li class="lookup">Lookup Tables
+    <ul class="dropdown submenu">
+        <li class="Products">Products</li>
+        <li class="Location">Location</li>
+        <li>Supplier</li>
+        <li>Customer</li>
+    </ul>
+</li>`;
+
+document.getElementById("menuNavBar").innerHTML = menuItems;
+document.getElementById('menuSideBar').innerHTML=`<div class="close-sidebar">✖</div>`+menuItems
+
 const hamburger = document.querySelector('.hamburger');
 const sidebar = document.querySelector('.sidebar');
 const closeSidebar = document.querySelector('.close-sidebar');
